018c9b7be4e9dfa30f697bbef35caaf53271b6ac
Sunil Ranahandola <sranahan@in.ibm.com>
2026-08-24 04:29:57 -0700

    Add env vars setup file to repo

---
+++ non_docker_build.sh
@@ -0,0 +1,10 @@
+#Source this file to setup the build env variables
+
+export SYNC_HOME=<USER_HOME>/SyncWeave
+export JAVA_HOME=${SYNC_HOME}/adks/ibm/jdk/jdk-21.0.12+8
+export PATH=${JAVA_HOME}/bin:$PATH
+
+export TOOLS_HOME=${SYNC_HOME}/tools
+
+export ANT_HOME=/home/sunil/sdi_build/ant/apache-ant-1.10.17
+export PATH=$ANT_HOME/bin:$PATH
